a6f160ab4f0a98bd6ea6f00f382ea7713799cee7
Dangyra <dangyra.tadaraite@gmail.com>
2023-11-26 23:25:27 +0200

    First auto_sistema repository

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// SIDEBAR TOGGLE
+let sidebarOpen = false;
+const sidebar = document.getElementById('sidebar');
+
+function openSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add('sidebar-responsive');
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove('sidebar-responsive');
+    sidebarOpen = false;
+  }
+}
+
+//TOGGLE MENU
+function menuToggle(){
+  const toggleMenu = document.querySelector('.menu');
+  toggleMenu.classList.toggle('active')
+}
