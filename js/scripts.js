f4e5ce9675ab1573cebeb3842c4791906f3489dc
Dangyra <dangyra.tadaraite@gmail.com>
2023-11-27 22:32:27 +0200

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,20 @@ function menuToggle(){
   const toggleMenu = document.querySelector('.menu');
   toggleMenu.classList.toggle('active')
 }
+
+//rezervation modal
+// Function to open the reservation modal
+document.addEventListener('DOMContentLoaded', function() {
+  const reservationButton = document.querySelector('#reservationButton');
+  const reservationModal = document.querySelector('#reservationModal');
+
+  reservationButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default behavior of the button
+    reservationModal.style.display = 'flex';
+  });
+});
+
+// Function to close the reservation modal
+function closeReservationModal() {
+  document.getElementById('reservationModal').style.display = 'none';
+}
